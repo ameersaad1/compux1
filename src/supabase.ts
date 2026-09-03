74eead48304f84aea3cfee4178d462e71853a054
ameersaad1 <amooshzero@gmail.com>
2026-09-03 06:26:32 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://dmhumdxefgzqmuysudqi.supabase.co'
-const supabaseAnonKey = sb_publishable_sg2irjycWPou6lGKzjFKVQ_fwp8ho92'
+const supabaseAnonKey = 'sb_publishable_sg2irjycWPou6lGKzjFKVQ_fwp8ho92'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
