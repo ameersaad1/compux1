ad68ac90cec0c610f9728a04a63ec9e96b7573c7
ameersaad1 <amooshzero@gmail.com>
2026-09-03 05:40:07 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'رابط_مشروعك_من_سوبابيس'
+const supabaseAnonKey = 'مفتاح_ANON_KEY_الخاص_بمشروعك'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
