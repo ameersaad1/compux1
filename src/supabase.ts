2dc069ff7afdc8a793a5f3334d0ed315362549ac
ameersaad1 <amooshzero@gmail.com>
2026-09-03 05:48:33 +0300

    Update Supabase URL and ANON key

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'رابط_مشروعك_من_سوبابيس'
-const supabaseAnonKey = 'مفتاح_ANON_KEY_الخاص_بمشروعك'
+const supabaseUrl = 'https://dmhumdxefgzqmuysudqi.supabase.co'
+const supabaseAnonKey = sb_publishable_sg2irjycWPou6lGKzjFKVQ_fwp8ho92'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
